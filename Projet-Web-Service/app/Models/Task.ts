7fe7bfe1380b78df8ce498cdeee2255b97174091
Lucaskoellsch <73243828+Lucaskoellsch@users.noreply.github.com>
2024-03-27 18:34:36 +0100

    Add files via upload
    Authentification

---
+++ Projet-Web-Service/app/Models/Task.ts
@@ -0,0 +1,24 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Task extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @column()
+  public name: string
+
+  @column()
+  public is_done: boolean
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+}

b26b0ac1c870dedc6496b4e385823cc344cd2877
Seillemar <133703176+Seillemar@users.noreply.github.com>
2024-03-31 18:23:23 +0200

    mails reservation

--- api-reservation/config/mail.ts
+++ api-reservation/config/mail.ts
@@ -3,32 +3,28 @@ import { defineConfig, transports } from '@adonisjs/mail'
 
 const mailConfig = defineConfig({
   default: 'smtp',
-
-   /**
-    * The mailers object can be used to configure multiple mailers
-    * each using a different transport or same transport with different
-    * options.
-   */
-  mailers: { 
+  mailers: {
     smtp: transports.smtp({
       host: env.get('SMTP_HOST'),
       port: env.get('SMTP_PORT'),
-			/**
-       * Uncomment the auth block if your SMTP
-       * server needs authentication
-       */
-      /* auth: {
+      secure: false,
+
+      auth: {
         type: 'login',
-        user: env.get('SMTP_USERNAME'),
-        pass: env.get('SMTP_PASSWORD'),
-      }, */
-    }),
-		     
+        user: env.get('SMTP_USERNAME') ?? '',
+        pass: env.get('SMTP_PASSWORD') ?? ''
+      },
+
+      tls: {},
+
+      ignoreTLS: false,
+      requireTLS: false,
+
+      pool: false,
+      maxConnections: 5,
+      maxMessages: 100,
+    })
   },
 })
 
 export default mailConfig
-
-declare module '@adonisjs/mail/types' {
-  export interface MailersList extends InferMailers<typeof mailConfig> {}
-}
